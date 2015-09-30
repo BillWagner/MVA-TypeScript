49fa3e85fa586fbdbe92cfe44e4b7d5f9b7f8566
Bill Wagner <bill@thebillwagner.com>
2015-09-30 09:58:26 -0400

    Create the basic controller.

---
+++ ngTypeScript/ngLabels/App_Scripts/labelCollection.ts
@@ -0,0 +1,15 @@
+﻿/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="labeleditor.ts" />
+
+module LabelApplication {
+
+    export class LabelCollection {
+        constructor(
+            private $scope: ng.IScope) {
+        }
+
+    }
+    LabelEditor
+        .editorModule
+        .controller("labelCollection", ["$scope", LabelCollection]);
+}
