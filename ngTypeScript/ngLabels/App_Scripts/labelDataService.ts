2202209f87830ebf48b5ae4ae87e8299c80b7249
Bill Wagner <bill@thebillwagner.com>
2015-09-30 17:12:45 -0400

    create the wrapper type for the resource object
    See notes for discussion items

---
+++ ngTypeScript/ngLabels/App_Scripts/labelDataService.ts
@@ -0,0 +1,29 @@
+﻿/// <reference path="label.d.ts" />
+/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../scripts/typings/angularjs/angular-resource.d.ts" />
+
+module LabelApplication {
+    import Rest = LabelApplication.Rest;
+    import ngr = ng.resource;
+
+    LabelEditor.editorModule.factory('labelDataService', ["$resource",
+        (r) => {
+            return new LabelDataService(r);
+        }]);
+
+    export class LabelDataService {
+        private resource: ngr.IResourceClass<ngr.IResource<Rest.Label>>;
+
+        constructor($resource: ngr.IResourceService) {
+            this.resource = $resource(
+                "api/Labels/:id",
+                { id: "@id" },
+                {
+                    get: { method: "GET" },
+                    save: { method: "PUT" },
+                    query: { method: "GET", isArray: true },
+                    delete: { method: "DELETE" }
+                });
+        }
+    }
+}
