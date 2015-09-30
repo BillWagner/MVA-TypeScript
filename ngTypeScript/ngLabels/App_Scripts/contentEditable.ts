098661969ddc9809126980bfdd4487306db5b1ee
Bill Wagner <bill@thebillwagner.com>
2015-09-30 11:26:28 -0400

    Add the method for the link function

--- ngTypeScript/ngLabels/App_Scripts/contentEditable.ts
+++ ngTypeScript/ngLabels/App_Scripts/contentEditable.ts
@@ -10,6 +10,16 @@ module LabelApplication {
         restrict = 'A';
         require = 'ngModel';
 
-
+        link = (scope: ng.IScope, element: ng.IRootElementService, attrs: any, ngModel: ng.INgModelController) => {
+            function read() {
+                ngModel.$setViewValue(element.html());
+            }
+            ngModel.$render = function () {
+                element.html(ngModel.$viewValue || "");
+            };
+            element.bind("blur keyup change", function () {
+                scope.$apply(read);
+            });
+        };        
     }
 }
