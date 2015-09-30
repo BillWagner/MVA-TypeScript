8f58a2bff38249ad98ff004a173d57a762b34a69
Bill Wagner <bill@thebillwagner.com>
2015-09-30 11:22:48 -0400

    Add the factory method

--- ngTypeScript/ngLabels/App_Scripts/contentEditable.ts
+++ ngTypeScript/ngLabels/App_Scripts/contentEditable.ts
@@ -1,6 +1,10 @@
 ﻿/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="labeleditor.ts" />
 
 module LabelApplication {
     class ContentEditable implements ng.IDirective {
+        static factory(): ng.IDirectiveFactory {
+            return () => new ContentEditable();
+        }
     }
 }
