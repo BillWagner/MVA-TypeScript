69c4f79d5db65ddafdba349cbe87db439584cf29
Bill Wagner <bill@thebillwagner.com>
2015-10-06 07:35:05 -0700

    Create our component.
    The TS file uses some experimental stuff, so it won't compile cleanly at
    this point.

---
+++ vNext/src/vNext/wwwroot/label-editor.ts
@@ -0,0 +1,17 @@
+﻿import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'label-editor'
+})
+@View({
+    template: '<h1>Welcome to {{title}}</h1>'
+})
+export class LabelEditor {
+    title: string;
+
+    constructor() {
+        this.title = "Label Editor";
+    }
+}
+
+bootstrap(LabelEditor); 
