a51aeec5b46b92c334fde7ec2ce591a4d287071b
Bill Wagner <bill@thebillwagner.com>
2015-10-06 23:08:22 -0700

    create the app and index file

--- vNext/src/vNext/wwwroot/app.ts
+++ vNext/src/vNext/wwwroot/app.ts
@@ -1,1 +1,18 @@
-﻿
+﻿/// <reference path="../typings/angular2/angular2.d.ts" />
+
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'next-app'
+})
+@View({
+    template: '<h1>Introducting {{ phrase }}</h1>'
+})
+// Component controller
+class MyAppComponent {
+    phrase: string;
+
+    constructor() {
+        this.phrase = 'Angular 2.0 and TypeScript';
+    }
+}
